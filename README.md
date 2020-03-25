ebdfac78cf12428072f8da5e1fcb6e4c08a92936
Iria Pantazi <iria.a.pantazi@gmail.com>
2020-03-25 16:44:11 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# **Utility for COVID-19 data prediction**
+---
+
+This utility is a small program written in Python 
+that produces plots for the data of COVID-19
+available through: 
+`https://pomber.github.io/covid19/timeseries.json`.
+
+## **Getting started**
+
+### **Prerequisites**
+The program is written in Python 3.8 and can run 
+with Python versions 3.6 and 3.7. Older versions
+have not been tested, and the program
+exits if Python 2.7 or older is detected.
+It is suggested that the user creates a virtual environment. 
+Such packages for creating and using virtual environments are 
+[mkvirtualenv](https://realpython.com/python-virtual-environments-a-primer/) 
+and [pyenv](https://realpython.com/intro-to-pyenv/).
+After initialising a virtual environment, the user 
+has to install the package requirements with the command 
+`pip install -r requirements.txt`.
+
+
+## **Information on input and output**
+The program provides a  variation of arguments that can be parsed. 
+These are:
+1. `-c` for providing the country name(s).
+2. `-d` for plotting the cumulative deaths per day.
+3. `-r` for plotting the cumulative recovered cases per day.
+4. `-o` for plotting the cumulative confirmed cases per day.
+
+These options are also available through the command 
+`./covid-stats.py -h`.
+
+
+### **Example output**
+
+Requesting the number of deaths `./covid-stats.py -d -c Greece Italy` example output:
+![image](img/trainsLinesStatus_output.png "trainsLinesStatus example output")
+
+## **Authors** 
+If you have any suggestions/corrections, 
+please contact [Iria Pantazi](iria.a.pantazi@gmail.com).
+
+
---
+++ covid-stats.py
@@ -0,0 +1,137 @@
+#! /usr/bin/env python
+
+import argparse
+import os.path
+import requests
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# constants
+URL = 'https://pomber.github.io/covid19/timeseries.json'
+COUNTRIES_CSV = 'countries.csv'
+
+
+def parse_arguments():
+    """
+    parse_arguments:
+    Args: -
+    Returns: args [str]
+    This function returns the appropriate arguments.
+    """
+    parser = argparse.ArgumentParser(
+                description='This program takes data concerning'
+                'COVID-19 confirmed cases, and creates plots.'
+                )
+    parser.add_argument('-c', '--countries', nargs='+', 
+                help='entrer the name(s) of country(-ies)')
+    parser.add_argument('-d', '--deaths', action='store_true', 
+                help='to plot deaths')
+    parser.add_argument('-r', '--recovered', action='store_true', 
+                help='to plot recovered')
+    parser.add_argument('-o', '--confirmed', action='store_true', 
+                help='to plot confirmed cases')
+    args = parser.parse_args()
+    return(args)  
+
+
+def choose_title(mode):
+    """
+    choose_title:
+    Args: mode [str]
+    Returns: title [str]
+    """
+    if mode == 'deaths':
+        return(f'Plot of number of cumulative deaths')
+    elif mode == 'recovered':
+        return(f'Plot of number of cumulative recovered cases')
+    elif mode == 'confirmed':
+        return(f'Plot of number of cumulative confirmed cases')
+
+
+
+def request_data(entries, mode):
+    """
+    request_data:
+    Args: entries [list], mode [str]
+    Returns: -
+    This function requests data of countries
+    contained in entries list with the mode
+    that has been parsed.
+    """
+    r = requests.get(URL)
+    data = r.json()
+    
+    # create countries csv file
+    if not os.path.isfile(COUNTRIES_CSV):
+        create_countries_csv(data)
+
+    # initiate plot
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_yscale('log')
+    x = np.arange(len(data.get('Afghanistan')))
+
+    # extract y-axis data
+    for ent in entries:
+        res = data.get(ent)
+        if (res == None):
+            print(f'There is no country named {ent}.'
+                    f'Please refer to {COUNTRIES_CSV} for'
+                    f'a list of valid countries.')
+            continue
+        mode_entries = []
+        for dt in res:
+            mode_entries.append(dt.get(mode))
+        plt.plot(x, mode_entries, 'o-',label=ent)
+    plt.ylim(ymin=1.0)
+    plt.legend(frameon=False)
+    plt.title(choose_title(mode))
+    plt.show()
+
+
+def create_countries_csv(data):
+    """
+    create_countries_csv:
+    Args: data [dist]
+    Returns: -
+    This function creates a csv file
+    with the names of all countries if
+    it does not exist.
+    """
+    countries = list(data.keys())
+    with open(COUNTRIES_CSV, 'w') as f:
+        for cnt in countries:
+            f.write(cnt)
+            f.write('\n')
+
+
+def main():
+    """
+    main:
+    Args: -
+    Returns: -
+    This function calls the appropriate
+    functions according to the parsed
+    arguments.
+    """
+    args = parse_arguments()
+
+
+    if args.deaths:
+        mode = 'deaths'
+    elif args.recovered:
+        mode = 'recovered'
+    elif args.confirmed:
+        mode = 'confirmed'
+    #else:
+    #    mode = 'deaths'
+
+
+    if args.countries:
+        request_data(args.countries, mode='deaths')
+
+
+if __name__ == "__main__":
+    main()
